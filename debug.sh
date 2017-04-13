1e4a52ff8016597e3ec4d1ce6b9a58b8edc39312
liyl <liyunlong880325@gmail.com>
2017-04-13 18:47:59 +0800

    Bug #00006 add log at analyse/c
        [bug number  ] 00006
        [root cause  ]
        [changes     ]
        [side effects]
        [reviewers   ]
    
    Signed-off-by: liyl <liyunlong880325@gmail.com>

--- debug.sh
+++ debug.sh
@@ -9,4 +9,9 @@
 b main
 b encoder/encoder.c:3753
 b encoder/lookahead.c:116
+b x264_slicetype_decide
+b x264_slicetype_analyse
+b scenecut
+b scenecut_internal
+b x264_slicetype_frame_cost
 run -v --threads 1  --pass 1 --bitrate 2000 -o test.264 --input-res 1280x720 --input-csp i420 ./1280_720_frame_10.yuv 
